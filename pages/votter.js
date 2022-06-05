be2e695a4d81335ec3279b415d21ff6fd5fb2df6
dulkifal <dulkifilibacker@gmail.com>
2022-06-05 09:38:02 +0530

    conslole

--- pages/votter.js
+++ pages/votter.js
@@ -1,30 +1,55 @@
-export default function votter(){
-  const users = [
-    {
-      id: 1,
-      Name: "Nizam",
-      sem: "1st",
-    }
+import initFirebase from '../utils/config';
+import { getFirestore } from "firebase/firestore";
+const db = getFirestore(initFirebase());
 
-  ]
-  return(
+import { collection,getDocs } from "firebase/firestore";
+
+import { useState } from 'react/';
+
+const arr = []
+
+export default  function votter() {
+  const [arrs , setarr] = useState([]);
+   
+  return (
     <div>
       <h1>Votters</h1>
-<p>who votted</p>
-    {
-      users.map(user => (
-        <table>
-          <tr>
-            <td>{user.id}</td>
-            <td>{user.Name}</td>
-            <td>{user.sem}</td>
-          </tr>
-           
-        </table>
-      ))
-    }
-  
-      </div>
+      <p>who votted</p>
+      <button
+        onClick={async () => {
+          const users = await getDocs(collection(db, "users"));
+          users.forEach((user) => {
+            arr.push(user.data())
+             
+          
+          });
+          // console.log(arr)
+          setarr(arr)
+        }}
+      >Get Total</button>
+      {           
+         console.log(arrs) &
+        arrs.forEach(user => (
+          console.log(user.Name)&
+          <table className="table">
+            <tr>
+              <td>{user.Name}</td>
+              <td>{user.sem}</td>
+              <td>{user.id}</td>
+            </tr>
+
+          </table>
+        ))
+      }
+    
+
+    </div>
 
   );
+}
+
+async function getVotter(){
+  const users = await getDocs(collection(db, "users"));
+  return users
+
 }
